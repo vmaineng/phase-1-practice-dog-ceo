404ec759b8f687a7a0cc5f8f47637e61239bb7fd
Mai Vang <vmaineng@gmail.com>
2022-05-04 16:12:56 -0700

    commit tree 5e79505f24659077a6ffed2d88cb3977199b2215

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log('%c HI', 'color: firebrick')
+//console.log('%c HI', 'color: firebrick')
+
+document.addEventListener('DOMContentLoaded', () => {
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+fetch (imgUrl).then(r => r.json()) //gets a json-ify data
+.then (doggyData => {   //added {}; keeps seeing the data 
+    renderImages(doggyData.message)
+})
+
+
+const dogContainer = document.querySelector('#dog-image-container')
+
+const renderImages = (doggyArray) => {
+    //console.log(doggyArray);
+    //use forEach bc it goes through everything and run some type of behavior for this array
+    //forEach passes a callback function, then one at a time, one elment goes through the callback function, and runs its own entirety
+    
+    doggyArray.forEach( (dogImage)=> {
+        //callback function receives each image once at a time
+        const image = document.createElement('img')
+        //hey element, create this new image
+        image.src = dogImage
+        //it's already an image URL and not an object
+        dogContainer.append(image)
+    });
+}
+
+} );
